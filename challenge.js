22e06ba34a63b2e88865eecfc7eceba18411dc5e
tobas <tomas.baska@outlook.com>
2021-11-05 23:40:19 +0100

    challege #2

--- challenge.js
+++ challenge.js
@@ -16,6 +16,43 @@ function checkDogs(dogsJulia, dogsKate) {
     })
 }
 
-checkDogs(dogsJulia, dogsKate);
-console.log(`test data 2`)
-checkDogs(dogsJulia2, dogsKate2);
+// checkDogs(dogsJulia, dogsKate);
+// console.log(`test data 2`)
+// checkDogs(dogsJulia2, dogsKate2);
+
+// challenge #2
+
+//1.
+const testData1 = [5, 2, 4, 1, 15, 8, 3];
+const testData2 = [16, 6, 10, 5, 6, 1, 4];
+
+function dogAge(dogs) {
+    // gets thru dogs array and calculate each dog's human age
+    return dogs.map(dog => {
+        // if is less than 2 uses one formula otherwise uses the other
+        return dog <= 2 ? 2 * dog : 16 + dog * 4;
+    });
+}
+
+const dogAges1 = dogAge(testData1);
+console.log(dogAges1);
+
+const dogAges2 = dogAge(testData2);
+console.log(dogAges2);
+
+//2.
+function reduceDogs(dogs) {
+    // gets rid of dogs with age less than 18 human age
+    return dogs.filter(dog =>  dog >= 18);
+}
+
+console.log(reduceDogs(dogAges1));
+console.log(reduceDogs(dogAges2));
+
+//3.
+function averageDogAge(dogs) {
+    // calculates average dog human age
+    return dogs.reduce((acc, dog) => acc + dog, 0) / dogs.length;
+}
+
+console.log(averageDogAge([...dogAges1, ...dogAges2]));
